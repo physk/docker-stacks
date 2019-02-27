e15e899077cd3dcc3d61085b4da2272ddd10c587
Physk <chris@physk.co.uk>
2019-02-27 11:39:13 +0000

    Create funkwhale.sh

---
+++ media_servers/funkwhale.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+mkdir -p ${BASE_DIR}/media_servers/funkwhale/
+cd ${BASE_DIR}/media_servers/funkwhale/
+touch .env
+echo "FUNKWHALE_HOSTNAME=${FUNKWHALE_SUBDOMAIN}.${DOMAIN}" >> .env
+echo "FUNKWHALE_PROTOCOL=http" >> .env
+echo "DJANGO_SECRET_KEY=$(openssl rand -hex 45)" >> .env
