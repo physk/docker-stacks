f839b83ebdf112aea895e3793799eb7afcb09bcc
Physk <chris@physk.co.uk>
2019-04-02 20:06:34 +0100

    Update setup.sh

--- services/setup.sh
+++ services/setup.sh
@@ -3,10 +3,6 @@
 echo "Creating docker network"
 docker network create traefik_proxy
 
-echo "Setting Permissions on ${BASE_DIR}"
-
-chown -hR ${PUID}:${PGID} ${BASE_DIR}
-
 echo "Creating base directory"
 sudo mkdir -p ${BASE_DIR}
 sudo chown -hR ${PUID}:${PGID} ${BASE_DIR}
@@ -18,3 +14,7 @@ mkdir -p ${BASE_DIR}/docker/shared
 echo "Copying traefik configs"
 cp templates/template_traefik.toml ${BASE_DIR}/services/traefik/traefik.toml
 touch ${BASE_DIR}/services/traefik/servers.toml
+
+echo "Updating ${BASE_DIR}/services/traefik/traefik.toml"
+sed -i "s#DOMAIN#${DOMAIN}#g" ${BASE_DIR}/services/traefik/traefik.toml
+sed -i "s#EMAIL#${CLOUDFLARE_EMAIL}#g" ${BASE_DIR}/services/traefik/traefik.toml
