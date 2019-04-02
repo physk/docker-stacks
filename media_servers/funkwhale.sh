707e8a09efd3186dc86a23ffeb217ad88e26a5d4
Physk <chris@physk.co.uk>
2019-04-02 19:41:32 +0100

    Upd

--- media_servers/funkwhale.sh
+++ media_servers/funkwhale.sh
@@ -5,3 +5,4 @@ touch .env
 echo "FUNKWHALE_HOSTNAME=${FUNKWHALE_SUBDOMAIN}.${DOMAIN}" >> .env
 echo "FUNKWHALE_PROTOCOL=http" >> .env
 echo "DJANGO_SECRET_KEY=$(openssl rand -hex 45)" >> .env
+echo "NESTED_PROXY=1" >> .env
